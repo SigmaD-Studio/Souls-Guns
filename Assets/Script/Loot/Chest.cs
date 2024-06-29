a3ee39f63bbe9522a9a5546ab290e311378ca190
DeadInside1213 <avenerairline@gmail.com>
2024-06-30 00:14:20 +0700

    them animation va trigger cho ruong

--- Assets/Script/Loot/Chest.cs
+++ Assets/Script/Loot/Chest.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class Chest : MonoBehaviour
 {
     [SerializeField]GameObject drop;
+    Animator ani;
+    Collision2D col;
+    private void Start()
+    {
+
+        ani = GetComponent<Animator>();
+        col = GetComponent<Collision2D>();  
+    }
+
     /*private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -17,12 +27,23 @@ public class Chest : MonoBehaviour
     }*/
 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*if (collision.gameObject.tag == "Player")
+        {*/
+            
+        
+    }
     private void Update()
     {
-     if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            GetComponent<LootBag>().InstanateLoot(drop.transform.position);
+            ani.SetTrigger("Open");
         }
-  
+    }
+
+    public void OpenChest()
+    {
+        GetComponent<LootBag>().InstanateLoot(drop.transform.position);
     }
 }
