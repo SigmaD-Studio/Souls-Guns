b46138188f55aec1c2ac5cf08134e8da8b9ef802
DeadInside1213 <avenerairline@gmail.com>
2024-07-13 16:37:13 +0700

    Enemy + ai

--- Assets/Script/EnemyAIv2/AiChase.cs
+++ Assets/Script/EnemyAIv2/AiChase.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class AiChase : MonoBehaviour
 {
-    public GameObject player;
+    
     public float speed;
 
+
     private float distance;
+
+
+    private Rigidbody2D rb;
+    private Animator ani;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();  
+        player = FindAnyObjectByType<PlayerController>().gameObject;
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,7 +27,28 @@ public class AiChase : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Flip(angle);
+        
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        float speeding = rb.velocity.magnitude;
+        ani.SetFloat("Speed", speed);
+
+        
+
     }
+
+    private void Flip(float angle)
+    {
+        if (angle > 90 || angle < -90)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 0f);
+        }
+        else if (angle < 90 || angle > -90)
+        {
+            transform.localScale = new Vector3(1f, 1f, 0f);
+        }
+    }
+
 }
