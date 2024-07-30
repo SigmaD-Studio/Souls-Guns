9cfc0109298e278ec3183714e7587dc1042cc986
DeadInside1213 <avenerairline@gmail.com>
2024-07-30 17:42:32 +0700

    Done Loading and Load Random Scene

--- Assets/Script/Misc/LoadScene.cs
+++ Assets/Script/Misc/LoadScene.cs
@@ -2,27 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
-    public Scene[] scenes;
-
-
+     
+    [SerializeField] Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
+        int RandomIndex = Random.Range(1, 4);
         
+        LoadingScene(RandomIndex);
+
 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadingScene(int sceneIndex)
     {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
 
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
     }
 
 
-    
 }
 
